19e39c12ee268bfe3daaa0fb48018ed7dd87c58d
wiper996100 <wiper996100@gmail.com>
2022-04-30 20:18:46 +0600

    Добавил библиотеки, создал глобальные стили

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import PostCards from './component/PostCards/PostCards';
 
-function App() {
+const FORM_DATA = new FormData();
+const REQUEST_OPTION = {
+  method: 'POST',
+  body: FORM_DATA,
+  redirect: 'follow'
+};
+const App = () => {
+  const [dataValue, setDataValue] = useState(null)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      FORM_DATA.append("actionName", "MessagesLoad");
+      fetch('http://f0665380.xsph.ru/?oldMessage=true', REQUEST_OPTION)
+        .then(data => data.json())
+        .then(data => setDataValue(data))
+    }, 5000)
+    return () => clearInterval(intervalId);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='row-revision'>
+      {dataValue === null ? <div className='spinner-loading-block'><CircularProgress color="inherit" /></div>
+        : dataValue.Messages.map((item) => (
+          <PostCards key={item.id} item={item} />
+        ))}
     </div>
   );
-}
+};
 
 export default App;
